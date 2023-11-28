d0223b05ccab18f7cab2bc491b233d3aee5d3128
Ninel35 <134783428+Ninel35@users.noreply.github.com>
2023-11-28 18:31:41 +0100

    hmw

--- src/components/Statistics/Statistics.jsx
+++ src/components/Statistics/Statistics.jsx
@@ -2,7 +2,7 @@ import css from './statistics.module.css';
 
 const Statistics = (props) => (
     <section className="statistics">
-        {props.title && <h2 className={css.title}>Upload stats</h2>}
+        {props.title && <h2 className={css.title}>{props.title }</h2>}
         <ul className={css.stat_list}>
             {props.stats.map(prop => (
                 <li key={prop.id} className={css.item}>
